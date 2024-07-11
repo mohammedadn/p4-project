efc9613856ffa4fc45b47e1ff0535143e63f9805
dickson4954 <dicksonmurithi155@gmail.com>
2024-07-11 10:06:20 +0300

    initial commit

---
+++ Frontend/src/Components/Login.js
@@ -0,0 +1,216 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false); // State to track if user is logged in
+  const [error, setError] = useState('');
+  const [formType, setFormType] = useState('login'); // Initial form type is login
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:5000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        setLoggedIn(true); // Set loggedIn state to true upon successful login
+      } else {
+        const data = await response.json();
+        setError(data.message || 'Invalid username or password');
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+      setError('Error logging in. Please try again.');
+    }
+  };
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:5000/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password, email }),
+      });
+
+      if (response.ok) {
+        setLoggedIn(true); // Set loggedIn state to true upon successful signup
+      } else {
+        const data = await response.json();
+        setError(data.message || 'Failed to signup');
+      }
+    } catch (error) {
+      console.error('Error signing up:', error);
+      setError('Error signing up. Please try again.');
+    }
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    setUsername('');
+    setPassword('');
+    setEmail('');
+    setError('');
+  };
+
+  const handleFormSwitch = () => {
+    setFormType(formType === 'login' ? 'signup' : 'login'); // Toggle formType between login and signup
+    setError(''); // Clear any error messages when switching forms
+  };
+
+  const handleFormSubmit = async (e) => {
+    try {
+      if (formType === 'login') {
+        await handleLogin(e); // Pass the event object to handleLogin
+      } else if (formType === 'signup') {
+        await handleSignup(e); // Pass the event object to handleSignup
+      }
+    } catch (error) {
+      console.error('Error handling form submission:', error);
+    }
+  };
+
+  return (
+    <div className="text-center">
+      {loggedIn ? (
+        <LoggedInContent username={username} handleLogout={handleLogout} />
+      ) : (
+        <div>
+          {formType === 'login' && (
+            <>
+              <h2>Login</h2>
+              <form onSubmit={handleFormSubmit}>
+                <div className="row justify-content-center">
+                  <div className="col-sm-6">
+                    <input
+                      type="text"
+                      className="form-control mb-3"
+                      placeholder="Username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row justify-content-center">
+                  <div className="col-sm-6">
+                    <input
+                      type="password"
+                      className="form-control mb-3"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row justify-content-center">
+                  <div className="col-sm-6">
+                    <button className="btn btn-primary me-2" type="submit">
+                      Login
+                    </button>
+                    <button
+                      className="btn btn-success"
+                      type="button"
+                      onClick={handleFormSwitch}
+                    >
+                      Switch to Sign Up
+                    </button>
+                  </div>
+                </div>
+                {error && (
+                  <div className="alert alert-danger mt-3">{error}</div>
+                )}
+              </form>
+            </>
+          )}
+          {formType === 'signup' && (
+            <>
+              <h2>Sign Up</h2>
+              <form onSubmit={handleFormSubmit}>
+                <div className="row justify-content-center">
+                  <div className="col-sm-6">
+                    <input
+                      type="text"
+                      className="form-control mb-3"
+                      placeholder="Username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row justify-content-center">
+                  <div className="col-sm-6">
+                    <input
+                      type="password"
+                      className="form-control mb-3"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row justify-content-center">
+                  <div className="col-sm-6">
+                    <input
+                      type="text"
+                      className="form-control mb-3"
+                      placeholder="Email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row justify-content-center">
+                  <div className="col-sm-6">
+                    <button className="btn btn-success me-2" type="submit">
+                      Sign Up
+                    </button>
+                    <button
+                      className="btn btn-primary"
+                      type="button"
+                      onClick={handleFormSwitch}
+                    >
+                      Switch to Login
+                    </button>
+                  </div>
+                </div>
+                {error && (
+                  <div className="alert alert-danger mt-3">{error}</div>
+                )}
+              </form>
+            </>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+function LoggedInContent({ username, handleLogout }) {
+  return (
+    <>
+      <h2>Welcome, {username}!</h2>
+      
+      <button className="btn btn-danger" onClick={handleLogout}>
+        Logout
+      </button>
+      <Link to="/home">Home</Link>
+      
+      
+
+    </>
+  );
+}
+
+export default Login;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,13 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Componets/Homepage';
+
+import Trainer from './Componets/Trainer';
+import Workout from './Componets/Workout';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  
+  {
+    path: '/home',
+    element: <Home />,
+  },
+ 
+  {
+    path: '/trainers',
+    element: <Trainer />,
+  },
+  {
+    path: '/workout',
+    element: <Workout />,
+  },
+
+  
+])
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
